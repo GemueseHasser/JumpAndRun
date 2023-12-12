85001fc7a8c5aa37f4dc4c583d8d5291195e15dc
Jonas Lobe <jonas.lobe.2006@outlook.com>
2023-12-12 21:50:06 +0100

    initial commit

---
+++ src/de/informatik/game/JumpAndRun.java
@@ -0,0 +1,27 @@
+package de.informatik.game;
+
+import javax.swing.JFrame;
+
+/**
+ * <p>Die Haupt- und Main-Klasse dieses Spiels. In dieser Klasse wird das Spiel sowohl instanziiert als auch
+ * initialisiert, weshalb sich die Main-Methode dieser Anwendung in der Klasse befindet, die als erstes von der JRE
+ * automatisch aufgerufen wird.</p>
+ * <p>Das Prinzip des Spiels gleicht dem eines gewöhnlichen Jump-and-Run spiels, bei dem man - abstrakt betrachtet -
+ * eine Figur hat, mit der man durch eine 2-Dimensionale Spielwelt läuft und verschiedenen statischen oder flexiblen
+ * Hindernissen ausweichen muss.</p>
+ */
+public class JumpAndRun {
+
+    //<editor-fold desc="main">
+
+    /**
+     * Die Main-Methode dieses {@link JumpAndRun Spiels}, welches als erstes von der JRE aufgerufen wird, um das
+     * Programm zu initialisieren.
+     *
+     * @param args Die Argumente, die von der JRE beim Start des Programms übergeben werden.
+     */
+    public static void main(final String[] args) {
+    }
+    //</editor-fold>
+
+}
